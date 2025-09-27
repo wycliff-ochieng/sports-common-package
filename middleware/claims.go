399f16faed84c0b54b16614eff45a6092155ea66
wycliff-ochieng <wyckieochieng91@gmail.com>
2025-09-27 14:03:49 +0300

    feat: add new shared claims struct

--- middleware/claims.go
+++ middleware/claims.go
@@ -2,13 +2,13 @@ package auth
 
 import (
 	"github.com/golang-jwt/jwt/v5"
-	"github.com/google/uuid"
+	//"github.com/google/uuid"
 )
 
 type Claims struct {
-	ID     int       `json:"id"`
-	UserID uuid.UUID `json:"userid"`
-	Roles  []string  `json:"roles"`
-	Email  string    `json:"email"`
+	ID     int      `json:"id"`
+	UserID string   `json:"sub"`
+	Roles  []string `json:"roles"`
+	Email  string   `json:"email"`
 	jwt.RegisteredClaims
 }
